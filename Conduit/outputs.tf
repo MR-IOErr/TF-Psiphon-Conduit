2af73876788f2474592b295a86213f31bf3c6b92
Arian Moafizad <arianmoafizad@gmail.com>
2026-01-31 11:11:09 +1100

    Delete old project

--- Conduit/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lightsail_instance_id" {
-  description = "ID of the Lightsail instance."
-  value       = aws_lightsail_instance.conduit.id
-}
-
-output "lightsail_instance_public_ip" {
-  description = "Public IP address of the Lightsail instance (if assigned)."
-  value       = aws_lightsail_instance.conduit.public_ip_address
-}
